aa98de9973060b7cf6fe6bbf0c4d5dcc06c7bc71
math039h <mathiasbrink@live.dk>
2024-09-01 21:10:38 +0200

    Git ignore

--- TicTacToe/TicTacToe/Program.cs
+++ TicTacToe/TicTacToe/Program.cs
@@ -8,3 +8,7 @@ var ticTacToe = new TicTacToe.TicTacToe(playerOne, playerTwo);
 ticTacToe.SetUp();
 
 ticTacToe.GameLoop();
+
+// Stop application from shutting down right away
+Console.WriteLine("Press any key to exit the application");
+Console.ReadLine();
